2ce4c37948f54c94854620acd14e9ee5b12eec56
bytefly007 <bytefly@innovationdreamtech.com>
2024-12-04 16:29:58 +0800

    init buckets

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,8 +3,70 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "byteflyDrive",
   access: (allow) => ({
-    "media/{entity_id}/*": [
+    "uploads/movies/{entity_id}/*": [
       allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+    "uploads/sounds/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+    "uploads/images/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+    "uploads/avatars/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+      allow.guest.to(["read"]),
+      allow.authenticated.to(["read"])
+    ],
+    "free/movies/*": [
+      allow.authenticated.to(["read"]),
+    ],
+    "free/sounds/*": [
+      allow.authenticated.to(["read"]),
+    ],
+    "free/images/*": [
+      allow.authenticated.to(["read"]),
+    ],
+    "subscribers/movies/*": [
+      allow.groups(["subscribers"]).to(["read"]),
+    ],
+    "subscribers/sounds/*": [
+      allow.groups(["subscribers"]).to(["read"]),
+    ],
+    "subscribers/images/*": [
+      allow.groups(["subscribers"]).to(["read"]),
+    ],
+    "purchased/movies/*": [
+      allow.groups(["purchasers"]).to(["read"]),
+    ],
+    "purchased/sounds/*": [
+      allow.groups(["purchasers"]).to(["read"]),
+    ],
+    "purchased/images/*": [
+      allow.groups(["purchasers"]).to(["read"]),
+    ],
+    "aiOutput/movies/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"])
+    ],
+    "aiOutput/sounds/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+    "aiOutput/images/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+    "posted/movies/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+      allow.authenticated.to(["read"]),
+      allow.guest.to(["read"]),
+    ],
+    "posted/sounds/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+      allow.authenticated.to(["read"]),
+      allow.guest.to(["read"]),
+    ],
+    "posted/images/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+      allow.authenticated.to(["read"]),
+      allow.guest.to(["read"]),
     ],
   }),
 });
